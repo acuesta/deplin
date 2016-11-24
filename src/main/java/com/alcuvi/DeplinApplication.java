9e05ae987077a603f3f8fcb8edca43f7c4c9ec2b
acuesta <alvaro.fouz@gmail.com>
2016-11-24 19:51:11 +0100

    Initial sprint boot start

---
+++ src/main/java/com/alcuvi/DeplinApplication.java
@@ -0,0 +1,12 @@
+package com.alcuvi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeplinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeplinApplication.class, args);
+	}
+}
