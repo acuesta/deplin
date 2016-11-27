20ce4f6de449364f47ef2d1d1c9602b9fc7474cc
acuesta <alvaro.fouz@gmail.com>
2016-11-27 13:48:29 +0100

    Instance integration test running

--- src/test/java/com/alcuvi/InstancesItTest.java
+++ src/test/java/com/alcuvi/InstancesItTest.java
@@ -7,8 +7,17 @@ import java.net.URL;
 import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DeplinApplication.class)
+@WebAppConfiguration
+@IntegrationTest("server.port:8080")
 public class InstancesItTest {
 
 	final static Logger logger = Logger.getLogger(InstancesItTest.class);
@@ -18,28 +27,26 @@ public class InstancesItTest {
 
 	@Test
 	public void getAllInstances() throws IOException, Exception {
-		final GetExecution getAllInstancesExecution = new GetExecution();
-		final HttpStatus status = getAllInstancesExecution.executeGet(GET_INSTANCES_URL);
+		final HttpStatus status = executeGet(GET_INSTANCES_URL);
 		Assert.assertEquals(HttpStatus.OK, status);
 	}
 
-	class GetExecution {
-
-		private HttpStatus executeGet(final String restURL) throws IOException, Exception {
-			HttpURLConnection conn = null;
-			try {
-				final URL url = new URL(restURL);
-				conn = (HttpURLConnection) url.openConnection();
-				conn.setRequestMethod("GET");
-				conn.setDoOutput(true);
-				conn.setDoInput(true);
-
-			} finally {
-				if (conn != null) {
-					conn.disconnect();
-				}
+	public HttpStatus executeGet(final String restURL) throws IOException, Exception {
+		HttpURLConnection conn = null;
+		HttpStatus status = null;
+		try {
+			final URL url = new URL(restURL);
+			conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setDoOutput(true);
+			conn.setDoInput(true);
+			status = HttpStatus.valueOf(conn.getResponseCode());
+			
+		} finally {
+			if (conn != null) {
+				conn.disconnect();
 			}
-			return HttpStatus.valueOf(conn.getResponseCode());
 		}
+		return status;
 	}
 }
