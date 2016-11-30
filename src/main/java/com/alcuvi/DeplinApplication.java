fd2296eb266259f656928587e4f3268b2c584f7c
acuesta <alvaro.fouz@gmail.com>
2016-11-30 22:07:12 +0100

    extract mongo and spring configuration file

--- src/main/java/com/alcuvi/DeplinApplication.java
+++ src/main/java/com/alcuvi/DeplinApplication.java
@@ -1,30 +1,12 @@
 package com.alcuvi;
 
-import java.net.UnknownHostException;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.core.MongoTemplate;
-
-import com.mongodb.Mongo;
-import com.mongodb.MongoClient;
 
 @SpringBootApplication
 public class DeplinApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeplinApplication.class, args);
-	}
-
-	/*
-	 * Use the standard Mongo driver API to create a com.mongodb.Mongo instance.
-	 */
-	public @Bean Mongo mongo() throws UnknownHostException {
-		return new MongoClient("localhost");
-	}
-
-	public @Bean MongoTemplate mongoTemplate() throws Exception {
-		return new MongoTemplate(mongo(), "instances");
 	}
 }
