9c8c381dda836371df4c5e4a5f5e3b9819a8df75
acuesta <alvaro.fouz@gmail.com>
2016-12-04 00:54:02 +0100

    test travis comilation fails

--- src/main/java/com/alcuvi/DeplinApplication.java
+++ src/main/java/com/alcuvi/DeplinApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DeplinApplication {
+public class DeplinAppcation {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeplinApplication.class, args);
